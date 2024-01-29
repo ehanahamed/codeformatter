6607af44e12dbb82ad24edf992079fa0ca5dbaef
Ehan <ehanahamed@ehan.dev>
2024-01-29 13:36:43 +0000

    yea

--- temp/main.js
+++ temp/main.js
@@ -1,9 +1,10 @@
 const acorn = require("acorn");
 
-let result = acorn.parse("1 + 1");
-
-console.log(result);
-console.log("e");
-for (let token of acorn.tokenizer("1 + 1")) {
-    console.log(token)
+for (let token of acorn.tokenizer("if ((1+1) == 2) { console.log(1 + 1); }")) {
+    //console.log(token.value + " y " + token.type.label)
+    if (token.value) {
+        console.log(token.value);
+    } else {
+        console.log(token.type.label);
+    }
 }
