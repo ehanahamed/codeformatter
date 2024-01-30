27a4ac835a3d9b8518cb25efed224cd2d3fe27ac
Ehan <ehanahamed@ehan.dev>
2024-01-30 14:22:56 +0000

    idk

---
+++ temp/cli.js
@@ -0,0 +1,5 @@
+#! /usr/bin/env node
+const fs = require("node:fs");
+const formaat = require("./main.js");
+
+console.log(formaat.func());
--- temp/main.js
+++ temp/main.js
@@ -1,16 +1,21 @@
 const acorn = require("acorn");
 
-var result = [];
-var tokens = [...acorn.tokenizer(
-    "if ((1+1) == 2) { console.log(1 + 1); }",
-    { "ecmaVersion": "6"}
-)];
-for (var i = 0; i < tokens.length; i++) {
-    if (tokens[i].value) {
-        result.push(tokens[i].value + " ");
-    } else {
-        result.push(tokens[i].type.label);
+var formaat = {
+    func: function () {
+        var result = [];
+        var tokens = [...acorn.tokenizer(
+            "if ((1+1) == 2) { console.log(1 + 1); }",
+            { "ecmaVersion": "6"}
+        )];
+        for (var i = 0; i < tokens.length; i++) {
+            if (tokens[i].value) { 
+                result.push(tokens[i].value + " ");
+            } else {
+                result.push(tokens[i].type.label);
+            }
+        }
+        return result;
     }
 }
 
-console.log(result);
+module.exports = formaat;
