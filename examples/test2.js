c0af5fca0259b7c5dc62d63f707d75b102e6037c
Ehan <ehanahamed@ehan.dev>
2024-02-12 16:31:57 +0000

    add new test(s)

---
+++ examples/test2.js
@@ -0,0 +1,13 @@
+var arraything = [
+    "string",
+    0,
+    1,
+    "another string",
+    "string w \n two lines!"
+]
+
+for (var i = 0; i < 2; i++) {
+    console.log(
+        arraything[i]
+    );
+}
